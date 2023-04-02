57d86f4a30030a04419bc149b8061d6af5dc926d
Ido Shemi <shemiido@gmail.com>
2023-04-02 11:20:57 +0300

    added registration and forgotpassword

---
+++ project/Project1/Project1/forgotPassword.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Project1
+{
+    public partial class forgotPassword : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGetCode_func(object sender, EventArgs e)
+        {
+            // get from the database the user
+            // check if exists and send mail according 
+            // don't forget input validation
+
+        }
+    }
+}
